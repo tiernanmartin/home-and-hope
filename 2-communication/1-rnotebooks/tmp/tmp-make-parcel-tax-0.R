69fd472381ad06a268b32134c5e3b6db07c8129c
Tiernan Martin <tiernan.martin@gmail.com>
2017-10-26 19:37:04 +0000

    update the creation script for parcel-tax-0

--- 2-communication/1-rnotebooks/tmp/tmp-make-parcel-tax-0.R
+++ 2-communication/1-rnotebooks/tmp/tmp-make-parcel-tax-0.R
@@ -74,9 +74,39 @@ acct_load <-
                },
                {read_csv(acct_fp)}) 
 
+tr_fp <- "./1-data/3-interim/kc-tax-reason.rds"
+
+tax_reason <- 
+  make_or_read(tr_fp,
+               {
+                 dr_id <- as_id("0B5Pp4V6eCkhrQzdjY0hKVUhOR3M")
+                 
+                 drive_read(dr_id = dr_id,
+                            .tempfile = FALSE,
+                            path = tr_fp,
+                            read_fun = read_rds)
+                 
+               },
+               {read_rds(tr_fp)})
+
+ts_fp <- "./1-data/3-interim/kc-tax-status.rds"
+
+tax_status <- 
+  make_or_read(ts_fp,
+               {
+                 dr_id <- as_id("0B5Pp4V6eCkhrWFlYLWM3RC1pRDA")
+                 
+                 drive_read(dr_id = dr_id,
+                            .tempfile = FALSE,
+                            path = ts_fp,
+                            read_fun = read_rds)
+                 
+               },
+               {read_rds(ts_fp)})
+
 acct <- 
   acct_load %>% 
-  rename_all(to_screaming_snake_case)
+  rename_all(to_screaming_snake_case) 
 
 rm(acct_load)
 gc()
@@ -140,12 +170,14 @@ p_ready <-
 
 acct_frmt <- 
   acct %>% 
-  mutate(PIN = str_c(MAJOR,MINOR)) %>% 
+  mutate(PIN = str_c(MAJOR,MINOR),
+         TAX_STATUS = tax_status[TAX_STAT],
+         TAX_REASON = tax_reason[TAX_VAL_REASON]) %>% 
   select(PIN, 
          ACCT_NBR,
          BILL_YR,
-         TAX_STATUS = TAX_STAT,
-         TAX_VAL_REASON,
+         TAX_STATUS,
+         TAX_REASON,
          APPR_LAND_VAL,
          APPR_IMPS_VAL,
          TAXABLE_LAND_VAL,
@@ -166,7 +198,7 @@ p_sf_ready <-
 tax_0_sf <- 
   list(acct_ready,p_ready,p_sf_ready) %>% 
   reduce(.f = inner_join, by = "PIN") %>% 
-  filter(TAX_STATUS %in% c("O", "X")) %>% 
+  filter(TAX_STATUS %!in% "Taxable") %>% 
   st_as_sf()
 
 
@@ -180,6 +212,3 @@ st_write(tax_0_sf,dsn = tax_0_fp, driver = "GPKG")
 drive_folder <- as_id("0B5Pp4V6eCkhrZ3NHOEE0Sl9FbWc")
 
 drive_upload(tax_0_fp, path = drive_folder)
-
-
-
