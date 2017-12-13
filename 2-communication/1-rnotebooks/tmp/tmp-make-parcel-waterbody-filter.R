20febf12518be59a1ab0f78da5d3124c4f98c231
Tiernan Martin <tiernan.martin@gmail.com>
2017-12-12 16:18:10 -0800

    calculate waterbody overlap pct

--- 2-communication/1-rnotebooks/tmp/tmp-make-parcel-waterbody-filter.R
+++ 2-communication/1-rnotebooks/tmp/tmp-make-parcel-waterbody-filter.R
@@ -44,17 +44,54 @@ w_id <- as_id("1OF2Z0sNWBmdDdZ4lPgUoolVELd7HCyWL")
 
 w <- drive_read(w_id, TRUE, read_fun, read_sf, stringsAsFactors = FALSE)
 
-w_union_nest <- w %>%
-  st_nest_sf %>% 
+w_union <- w %>% 
   st_set_crs(4326) %>% 
+  st_union() %>% 
   st_transform(2926) %>% 
-  st_union()
+  st_make_valid() 
 
 # Filters ----
 
+sample_pins <- c("7666200311","0468000050","9528100790","0225049061","4315701200","342403900109","2524039040")
+
+p_sample <- p_nest %>% 
+  mutate(PIN = map_chr(data,"PIN")) %>% 
+  filter(PIN %in% sample_pins)
+
+st_intersect_area <- function(x, y, crs){ 
+  
+  x_sfc <- x %>% 
+    st_sfc %>% 
+    st_set_crs(crs)
+  
+  area_x <- x_sfc %>% st_area() %>% as.double()
+  
+  area_xy <- st_intersection(x_sfc, y) %>% st_area %>% as.double()
+  
+  if(is_empty(area_xy)){return(as.double(0))}
+  
+  overlap_pct <- area_xy %>% 
+    divide_by(area_x) %>% 
+    as.double() %>% 
+    round(2)
+  
+  return(overlap_pct)
+}
+
+tmp <- p_sample %>% 
+  mutate(WATER_OVERLAP_PCT = map_dbl(geometry, st_intersect_area, y = w_union, crs = 2926)) %>% 
+  st_transform(4326)
 
 
+tic()
 
+p_overlap_fp <- root_file("./1-data/3-interim/p-no-water-sf.gpkg")
 
+p_overlap <- p_nest %>% 
+  mutate(WATER_OVERLAP_PCT = map_dbl(geometry, st_intersect_area, y = w_union, crs = 2926))
+
+st_write(obj = p_overlap, dsn = p_overlap_fp, driver = "GPKG", layer_options = "OVERWRITE=TRUE")
+
+toc()
 # Upload to Drive ----
 
