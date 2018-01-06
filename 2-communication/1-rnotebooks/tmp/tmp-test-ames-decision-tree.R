625e3721d3e3fbb004d4f19da0411884fe043ed8
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-06 12:01:36 -0800

    convert the data to simple features

--- 2-communication/1-rnotebooks/tmp/tmp-test-ames-decision-tree.R
+++ 2-communication/1-rnotebooks/tmp/tmp-test-ames-decision-tree.R
@@ -1,20 +1,28 @@
 # Setup ----
 
 library(tidyverse)
+library(sf)
+library(mapview)
 library(snakecase)
 library(AmesHousing)
 library(rpart)
 library(rattle)
+library(skimr)
 
 root <- rprojroot::is_rstudio_project
 root_file <- root$make_fix_file()
 options(httr_oob_default=TRUE) 
 htmltools::tagList(rmarkdown::html_dependency_font_awesome())
+Sys.setlocale("LC_CTYPE", "Chinese")
 
 # Access Data ----
 
-
+a <- make_ames() %>% 
+  rename_all(to_screaming_snake_case) %>% 
+  mutate(geometry = st_sfc(map2(LONGITUDE, LATITUDE, ~ st_point(c(.x,.y))))) %>% 
+  st_sf %>% 
+  st_set_crs(4326)
 
 # View Data ----
 
-
+mapview(a)
