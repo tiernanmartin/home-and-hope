a266baa27e0c0a93ab6101073ff81b99417c6d0c
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-06 17:26:09 -0800

    single family and pool variables added (WIP)

--- 2-communication/1-rnotebooks/tmp/tmp-test-ames-decision-tree.R
+++ 2-communication/1-rnotebooks/tmp/tmp-test-ames-decision-tree.R
@@ -1,14 +1,14 @@
 # Setup ----
-
-library(tidyverse) 
+library(skimr)
 library(scales)
+library(tidyverse) 
 library(sf)
 library(mapview)
 library(snakecase)
 library(AmesHousing)
 library(rpart)
-library(rattle)
-library(skimr)
+library(rattle) 
+library(janitor)
 library(devtools)
 
 Sys.setlocale("LC_CTYPE", "Chinese")     # use skim() on Windows (See README.md on pkg Github repo)
@@ -33,17 +33,45 @@ make_ames() %>% select_if(is.factor) %>% map(table)
 
 # VAR ONE: single-family ONLY
 
-# work in progress
+# Check out the crosstabulation of zoning and sub class
+
+a %>% 
+  split(.$MS_ZONING) %>% 
+  map(~ tabyl(.x, MS_SUB_CLASS, sort = TRUE))
+
+# get the most common sub classes from the single family zoning categories
+
+sf_zones <- c("Residential_Low_Density", "Residential_Medium_Density")
+
+sf_classes <- a %>% 
+  select(MS_ZONING,MS_SUB_CLASS) %>% 
+  filter(MS_ZONING %in% sf_zones) %>% 
+  group_by(MS_SUB_CLASS) %>% 
+  summarize(N = n()) %>% 
+  arrange(desc(N)) %>% 
+  slice(1:6) %>% 
+  transmute(MS_SUB_CLASS = as.character(MS_SUB_CLASS)) %>% 
+  pull(MS_SUB_CLASS)
+
+
+a_var_one <- a %>% 
+  mutate(MS_ZONING_CHR = as.character(MS_ZONING),
+         MS_SUB_CLASS_CHR = as.character(MS_SUB_CLASS)) %>% 
+  mutate(SF_LGL = if_else(MS_ZONING_CHR %in% sf_zones & MS_SUB_CLASS_CHR %in% sf_classes,TRUE,FALSE))
+
+glimpse(a_var_one)
 
 # VAR TWO: top-5 most expensive neighborhoods (sale price per lot square foot)
 
-test1 <- a %>% 
+a_var_two <- a_var_one %>% 
   mutate(PRICE_SQFT = dollar(SALE_PRICE/LOT_AREA),
          PRICE_SQFT_LOG = log10(SALE_PRICE)/log10(LOT_AREA)) %>% 
-  mutate(NHOOD_TOP5 = fct_reorder(NEIGHBORHOOD,PRICE_SQFT_LOG, .desc = TRUE,fun = mean),
+  mutate(PRICE_SQFT = if_else(SF_LGL,PRICE_SQFT,NA_character_),
+         PRICE_SQFT_LOG = if_else(SF_LGL, PRICE_SQFT_LOG, NA_real_)) %>% 
+  mutate(NHOOD_TOP5 = fct_reorder(NEIGHBORHOOD,PRICE_SQFT_LOG, .desc = TRUE,fun = mean, na.rm = TRUE),
          NHOOD_TOP5 = fct_other(NHOOD_TOP5, keep = levels(NHOOD_TOP5)[1:5])) 
 
-test1 %>% 
+a_var_two %>% 
   select(NEIGHBORHOOD,
          NHOOD_TOP5,
          SALE_PRICE,
@@ -53,8 +81,17 @@ test1 %>%
   arrange(NHOOD_TOP5, desc(PRICE_SQFT)) %>% 
   print(n = 200)
 
-a %>% 
-  mutate(PRICE_SQFT = SALE_PRICE/LOT_AREA) %>% 
-  group_by(NEIGHBORHOOD) %>% 
-  summarise(MEDIAN_SQFT_VAL = median(PRICE_SQFT)) %>% 
-  arrange(desc(MEDIAN_SQFT_VAL)) %>% print(n = Inf)
+# VAR THREE: have a pool
+
+a_var_two %>% 
+  filter(POOL_AREA > 0 ) %>% 
+  select(NEIGHBORHOOD,
+         NHOOD_TOP5,
+         SALE_PRICE,
+         LOT_AREA,
+         PRICE_SQFT,
+         PRICE_SQFT_LOG,
+         POOL_AREA) %>%  
+  print(n = 200)
+
+# Oh no! All of the top 5 neighborhoods get filtered out when we select for pools - shoot!
