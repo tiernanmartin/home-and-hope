af74059eecb33d7065c1256598bd500d28e53341
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-06 16:03:42 -0800

    find top 5 neighborhoods (by sale average sale price per sqft)

--- 2-communication/1-rnotebooks/tmp/tmp-test-ames-decision-tree.R
+++ 2-communication/1-rnotebooks/tmp/tmp-test-ames-decision-tree.R
@@ -1,6 +1,7 @@
 # Setup ----
 
-library(tidyverse)
+library(tidyverse) 
+library(scales)
 library(sf)
 library(mapview)
 library(snakecase)
@@ -8,12 +9,9 @@ library(AmesHousing)
 library(rpart)
 library(rattle)
 library(skimr)
+library(devtools)
 
-root <- rprojroot::is_rstudio_project
-root_file <- root$make_fix_file()
-options(httr_oob_default=TRUE) 
-htmltools::tagList(rmarkdown::html_dependency_font_awesome())
-Sys.setlocale("LC_CTYPE", "Chinese")
+Sys.setlocale("LC_CTYPE", "Chinese")     # use skim() on Windows (See README.md on pkg Github repo)
 
 # Access Data ----
 
@@ -25,4 +23,38 @@ a <- make_ames() %>%
 
 # View Data ----
 
+skim(a)
+
 mapview(a)
+
+make_ames() %>% select_if(is.factor) %>% map(table)
+
+# Create a decision tree ----
+
+# VAR ONE: single-family ONLY
+
+# work in progress
+
+# VAR TWO: top-5 most expensive neighborhoods (sale price per lot square foot)
+
+test1 <- a %>% 
+  mutate(PRICE_SQFT = dollar(SALE_PRICE/LOT_AREA),
+         PRICE_SQFT_LOG = log10(SALE_PRICE)/log10(LOT_AREA)) %>% 
+  mutate(NHOOD_TOP5 = fct_reorder(NEIGHBORHOOD,PRICE_SQFT_LOG, .desc = TRUE,fun = mean),
+         NHOOD_TOP5 = fct_other(NHOOD_TOP5, keep = levels(NHOOD_TOP5)[1:5])) 
+
+test1 %>% 
+  select(NEIGHBORHOOD,
+         NHOOD_TOP5,
+         SALE_PRICE,
+         LOT_AREA,
+         PRICE_SQFT,
+         PRICE_SQFT_LOG) %>% 
+  arrange(NHOOD_TOP5, desc(PRICE_SQFT)) %>% 
+  print(n = 200)
+
+a %>% 
+  mutate(PRICE_SQFT = SALE_PRICE/LOT_AREA) %>% 
+  group_by(NEIGHBORHOOD) %>% 
+  summarise(MEDIAN_SQFT_VAL = median(PRICE_SQFT)) %>% 
+  arrange(desc(MEDIAN_SQFT_VAL)) %>% print(n = Inf)
