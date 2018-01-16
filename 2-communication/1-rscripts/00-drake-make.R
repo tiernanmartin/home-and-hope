410dbb9b5cc1b47c4bcd69d90a908d787f1941b9
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-16 12:19:57 -0800

    first example of a complete make_*() function: make_acct()

--- 2-communication/1-rscripts/00-drake-make.R
+++ 2-communication/1-rscripts/00-drake-make.R
@@ -14,7 +14,7 @@ root <- rprojroot::is_rstudio_project
 root_file <- root$make_fix_file()
 options(httr_oob_default=TRUE) 
 htmltools::tagList(rmarkdown::html_dependency_font_awesome())
-# CREATE MAKE PLANS ----
+# MAKE PLANS ----
 
 parcels_plan <- make_plan(
   acct = make_acct(),
@@ -40,20 +40,46 @@ project_plan <- rbind(parcels_plan,
                      utilization_plan)    # rbind all plans together
 
 
+# COMMAND: GET_PUB ----
+
+get_pub <- function(){
+  
+}
+
 # COMMAND: MAKE_ACCT ----
 
-make_acct <- function(){
+make_acct <- function(){ 
   
   acct_fp <- root_file("./1-data/2-external/kc_real_prop_acct_extract.rds")
   
+  acct_dr_id <- as_id("19f4AUMAEshnDNJqGjVsurFthbKGcKYvh")
+  
   acct <- acct_fp %>% 
-    make_or_read({
-      dr_id <- as_id("19f4AUMAEshnDNJqGjVsurFthbKGcKYvh")
-      
-      drive_read(dr_id = dr_id,path = acct_fp,.tempfile = FALSE,read_fun = read_rds)
-      
-    },
-                 {read_rds(acct_fp)})
+    make_or_read2(dr_id = acct_dr_id,
+                  get_expr = {
+                    realprop <- read.socrata("https://data.kingcounty.gov/resource/mmfz-e8xr.csv",
+                                             email = "FAKE_NAME@FAKE_EMAIL.COM",
+                                             password = "FAKE_PASSWORD" # CHANGE TO REAL BEFORE RUNNING
+                    )
+                    
+                    r <- realprop %>% 
+                      rename_all(to_screaming_snake_case) 
+                    
+                    r_fp <- root_file("./1-data/2-external/kc_real_prop_acct_extract.rds")
+                    
+                    drive_folder_id <- as_id("0B5Pp4V6eCkhrdlJ3MXVaNW16T0U")
+                    
+                    write_rds(r,r_fp, compress = "gz")
+                    
+                    drive_upload(media = r_fp, path = drive_folder_id)
+                    
+                    # drive_update(as_id("19f4AUMAEshnDNJqGjVsurFthbKGcKYvh"), r_fp)
+                  },
+                  make_expr = {
+                    drive_read(dr_id = acct_dr_id,path = acct_fp,.tempfile = FALSE,read_fun = read_rds)
+                  },
+                  read_expr = {read_rds(acct_fp)})
+  
 }
 
 
@@ -75,5 +101,5 @@ make_acct <- function(){
 
 # COMMAND: MAKE_UTIL_POTENTIAL ---- 
 
-# RUN PLAN ----
+# RUN PROJECT PLAN ----
 make(project_plan)
