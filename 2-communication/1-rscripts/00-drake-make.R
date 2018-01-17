70d20a47bb68366fd5f932906a7a8e9ac8895827
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-17 13:22:52 -0800

    fixed failing interaction between drive_read() and readLines(); other minor fixes

--- 2-communication/1-rscripts/00-drake-make.R
+++ 2-communication/1-rscripts/00-drake-make.R
@@ -16,14 +16,14 @@ options(httr_oob_default=TRUE)
 htmltools::tagList(rmarkdown::html_dependency_font_awesome())
 # MAKE PLANS ----
 
-lookup_plan <- make_plan( 
+lookup_plan <- drake_plan( 
   lu = make_lu(),
   tax_status = make_tax_status(),
   tax_reason = make_tax_reason()
   
 )
   
-parcels_plan <- make_plan(
+parcels_plan <- drake_plan(
   acct = make_acct(),
   parcel_sf = make_parcel_df(acct, tax_status,tax_reason)
 )
@@ -66,6 +66,7 @@ parse_lu_string <- function(string, col_sep, row_sep, join_name, long_name){
 # COMMAND: MAKE_TAX_REASON ----
 
 make_tax_reason <- function(){
+   
   tax_r_fp <- root_file("1-data/1-raw/tax_reason.txt")
   
   tax_r_dr_id <- as_id("118oisrou6ieTP2WXpg8IE1dtmsybRhkT")
@@ -92,17 +93,17 @@ MX = more than one reason applies
                     
                     drive_update(tax_r_dr_id, tax_r_fp)
                   },
-                  make_expr = {
-                    drive_read(dr_id = tax_r_dr_id,path = tax_r_fp,.tempfile = FALSE,read_fun = readLines,con = tax_r_fp)
+                  make_expr = { 
+                    drive_download(file = tax_r_dr_id, path = tax_r_fp) 
+                    read_lines(tax_r_fp)
                   },
-                  read_expr = {readLines(tax_r_fp)})
+                  read_expr = {read_lines(tax_r_fp)})
   
   tax_reason <- 
     tax_reason_load %>% 
     str_c(collapse = "\n") %>% 
     parse_lu_string(col_sep = "\\s=\\s", row_sep = "\n",join_name = "TAX_REASON","TAX_REASON_DESC")
-}
-
+} 
 
 # COMMAND: MAKE_ACCT ----
 
