6d484681c7542a29e9efc9b9a258bd5b2708a513
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-16 17:30:33 -0800

    added lookup_plan and showed how its done w/ make_tax_reason()

--- 2-communication/1-rscripts/00-drake-make.R
+++ 2-communication/1-rscripts/00-drake-make.R
@@ -16,10 +16,16 @@ options(httr_oob_default=TRUE)
 htmltools::tagList(rmarkdown::html_dependency_font_awesome())
 # MAKE PLANS ----
 
+lookup_plan <- make_plan( 
+  lu = make_lu(),
+  tax_status = make_tax_status(),
+  tax_reason = make_tax_reason()
+  
+)
+  
 parcels_plan <- make_plan(
   acct = make_acct(),
-  parcels_pt = make_parcels_pt(acct),
-  parcels_poly = make_parcels_poly(parcels_pt)
+  parcel_sf = make_parcel_df(acct, tax_status,tax_reason)
 )
 
 suitability_plan <- drake_plan(
@@ -27,12 +33,14 @@ suitability_plan <- drake_plan(
   water_coverage = make_water(),
   uga = make_uga(),
   zoning = make_zoning(),
-  present_use = make_present_use()
+  present_use = make_present_use(),
+  parcel_suitability = make_suitability(parcel_sf, tax_e, water_coverage, uga, zoning, present_use)
 )
 
 utilization_plan <- drake_plan(
   util_present = make_util_present(),
-  util_potential = make_util_potential()
+  util_potential = make_util_potential(),
+  parcel_utilization = make_parcel_utilization(parcel_sf, util_present, util_potential)
 )
 
 project_plan <- rbind(parcels_plan,
@@ -40,11 +48,61 @@ project_plan <- rbind(parcels_plan,
                      utilization_plan)    # rbind all plans together
 
 
-# COMMAND: GET_PUB ----
-
-get_pub <- function(){
-  
+# FUNCTION: MAKE_PARSE_LU_STRING ----
+parse_lu_string <- function(string, col_sep, row_sep, join_name, long_name){ 
+  str_split(string, pattern = row_sep) %>% 
+    flatten() %>% 
+    keep(~ str_detect(.x,"")) %>%  
+    str_replace_all("\\\n","") %>% 
+    map_chr(c) %>% 
+    map_df(~.x %>% str_split(pattern = col_sep) %>% as.data.frame %>% t %>% as_data_frame ) %>%  
+    set_names(c(join_name,long_name))
 }
+
+# COMMAND: MAKE_LU ----
+
+# COMMAND: MAKE_TAX_STATUS ----
+
+# COMMAND: MAKE_TAX_REASON ----
+
+make_tax_reason <- function(){
+  tax_r_fp <- root_file("1-data/1-raw/tax_reason.txt")
+  
+  tax_r_dr_id <- as_id("118oisrou6ieTP2WXpg8IE1dtmsybRhkT")
+  
+  tax_reason_load <- tax_r_fp %>% 
+    make_or_read2(dr_id = tax_r_dr_id,
+                  get_expr = {
+                    
+                    string <- "
+FS = senior citizen exemption
+EX = exempt
+OP = operating
+NP = non profit exemption
+CU = open space exemption
+HI = home improvement exemption
+HP = historic property exemption
+MX = more than one reason applies
+"
+                    writeLines(string, tax_r_fp)
+                    
+                    drive_folder <- as_id("0B5Pp4V6eCkhrb1lDdlNaOFY4V0U")
+                    
+                    # drive_upload(media = tax_r_fp, path = drive_folder)
+                    
+                    drive_update(tax_r_dr_id, tax_r_fp)
+                  },
+                  make_expr = {
+                    drive_read(dr_id = tax_r_dr_id,path = tax_r_fp,.tempfile = FALSE,read_fun = readLines,con = tax_r_fp)
+                  },
+                  read_expr = {readLines(tax_r_fp)})
+  
+  tax_reason <- 
+    tax_reason_load %>% 
+    str_c(collapse = "\n") %>% 
+    parse_lu_string(col_sep = "\\s=\\s", row_sep = "\n",join_name = "TAX_REASON","TAX_REASON_DESC")
+}
+
 
 # COMMAND: MAKE_ACCT ----
 
@@ -71,9 +129,9 @@ make_acct <- function(){
                     
                     write_rds(r,r_fp, compress = "gz")
                     
-                    drive_upload(media = r_fp, path = drive_folder_id)
+                    # drive_upload(media = r_fp, path = drive_folder_id)
                     
-                    # drive_update(as_id("19f4AUMAEshnDNJqGjVsurFthbKGcKYvh"), r_fp)
+                    drive_update(as_id("19f4AUMAEshnDNJqGjVsurFthbKGcKYvh"), r_fp)
                   },
                   make_expr = {
                     drive_read(dr_id = acct_dr_id,path = acct_fp,.tempfile = FALSE,read_fun = read_rds)
@@ -82,11 +140,8 @@ make_acct <- function(){
   
 }
 
-
-# COMMAND: MAKE_PARCELS_PT ----
-
-# COMMAND: MAKE_PARCELS_POLY ----
-
+# COMMAND: MAKE_PARCELS_SF ----
+ 
 # COMMAND: MAKE_TAX_E ----
 
 # COMMAND: MAKE_WATER ----
@@ -97,9 +152,12 @@ make_acct <- function(){
 
 # COMMAND: MAKE_PRESENT_USE ----
 
+# COMMAND: MAKE_PARCEL_SUITABILITY ----
+
 # COMMAND: MAKE_UTIL_PRESENT ----
 
 # COMMAND: MAKE_UTIL_POTENTIAL ---- 
 
+# COMMAND: MAKE_PARCEL_UTILIZATION----
 # RUN PROJECT PLAN ----
 make(project_plan)
