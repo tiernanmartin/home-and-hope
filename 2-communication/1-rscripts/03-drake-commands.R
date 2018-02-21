d1fb553ef671ea9099c0863fe57dd73e200b14c8
Tiernan Martin <tiernan.martin@gmail.com>
2018-02-20 16:41:15 -0800

    Fix the CRS transformation that was causing problems with the polygons

--- 2-communication/1-rscripts/03-drake-commands.R
+++ 2-communication/1-rscripts/03-drake-commands.R
@@ -230,6 +230,8 @@ make_acct <- function(){
                   },
                   read_expr = function(fp){read_rds(fp)})
   
+  return(acct)
+  
 }
 
 
@@ -356,7 +358,7 @@ make_parcel_sf <- function(parcel_sf_poly){
   
   p_sf_2926 <- st_transform(parcel_sf_poly, 2926)
   
-  p_sf_2926$geom_pt <- st_centroid(st_geometry(p_sf_2926))
+  p_sf_2926$geom_pt <- st_centroid(st_geometry(p_sf_2926)) 
   
   p_sf_ready <- p_sf_2926 %>% 
     mutate(MAJOR = str_pad(MAJOR, 6, "left", "0"),
@@ -364,8 +366,8 @@ make_parcel_sf <- function(parcel_sf_poly){
     transmute(PIN = str_c(MAJOR,MINOR),
               geom_pt) %>% 
     drop_na() %>% 
-    st_transform(4326) %>% 
     st_set_geometry("geometry") %>% 
+    st_transform(2926) %>% 
     st_sf
   
   parcel_sf <- p_sf_ready
@@ -375,60 +377,61 @@ make_parcel_sf <- function(parcel_sf_poly){
 
 # COMMAND: MAKE_PARCEL_READY ----
 
-make_parcel_ready <- function(lu, prop_type, tax_status,tax_reason, pub_parcel, acct, parcel_addr, parcel_df, parcel_sf){
+make_parcel_ready <- function(lu, prop_type, tax_status, tax_reason, pub_parcel, acct, parcel_addr, parcel_df, parcel_sf){
    
+  
   # MAKE P_SF_READY
 
-  p_sf_ready <-  parcel_sf %>% 
-    miscgis::subset_duplicated("PIN") %>% 
-    group_by(PIN) %>%  # remove any duplicates PIN records with NAs
-    slice(1) %>%  # take the first record and discard the rest
-    ungroup %>% 
-    rbind(subset_duplicated(parcel_sf,"PIN",notin = TRUE)) %>% 
+  p_sf_ready <-  parcel_sf %>%
+    miscgis::subset_duplicated("PIN") %>%
+    group_by(PIN) %>%  
+    slice(1) %>%  
+    ungroup %>%
+    rbind(subset_duplicated(parcel_sf,"PIN",notin = TRUE)) %>%
     st_set_geometry("geometry")
-    
+
   # MAKE P_ADDR_READY
-  p_addr_ready <- parcel_addr %>% 
-    transmute(PIN, 
-              ADDRESS_FULL = ADDR_FULL, 
-              CITY_NAME = CTYNAME, 
+  p_addr_ready <- parcel_addr %>%
+    transmute(PIN,
+              ADDRESS_FULL = ADDR_FULL,
+              CITY_NAME = CTYNAME,
               CITY_NAME_POSTAL = POSTALCTYN,
-              ZIPCODE = factor(ZIP_5), 
-              PRIMARY_ADDR_LGL = as.logical(PRIM_ADDR)) %>% 
+              ZIPCODE = factor(ZIP_5),
+              PRIMARY_ADDR_LGL = as.logical(PRIM_ADDR)) %>%
     mutate(CITY_NAME = factor(case_when(
                 all(is.na(CITY_NAME),is.na(CITY_NAME_POSTAL)) ~ NA_character_,
                 is.na(CITY_NAME) ~ CITY_NAME_POSTAL,
                 TRUE ~ CITY_NAME
-              ))) %>% 
-  group_by(PIN) %>% 
-    arrange(desc(PRIMARY_ADDR_LGL)) %>% 
-    slice(1) %>% 
+              ))) %>%
+  group_by(PIN) %>%
+    arrange(desc(PRIMARY_ADDR_LGL)) %>%
+    slice(1) %>%
     ungroup
-  
-  
-  
+
+
+
   # MAKE P_READY
-  
-  present_use <- lu %>% 
-    filter(LU_TYPE == 102) %>% 
+
+  present_use <- lu %>%
+    filter(LU_TYPE == 102) %>%
     select(PRESENT_USE = LU_ITEM,
            PRESENT_USE_DESC = LU_DESCRIPTION)
-  
-  pub_parcel_ready <- pub_parcel %>% 
+
+  pub_parcel_ready <- pub_parcel %>%
     transmute(PIN,
               ASSESSOR_PUB_LIST_LGL = TRUE)
-  
-  p_ready <- parcel_df %>% 
+
+  p_ready <- parcel_df %>%
    mutate(MAJOR = str_pad(string = MAJOR,width = 6,side = "left",pad = "0"),
            MINOR = str_pad(string = MINOR,width = 4,side = "left",pad = "0"),
-           PIN = str_c(MAJOR,MINOR)) %>% 
-    left_join(prop_type, by = "PROP_TYPE") %>% 
-    left_join(present_use, by = "PRESENT_USE") %>% 
-    left_join(pub_parcel_ready, by = "PIN") %>% 
-    mutate(ASSESSOR_PUB_LIST_LGL = if_else(is.na(ASSESSOR_PUB_LIST_LGL),FALSE,ASSESSOR_PUB_LIST_LGL)) %>% 
+           PIN = str_c(MAJOR,MINOR)) %>%
+    left_join(prop_type, by = "PROP_TYPE") %>%
+    left_join(present_use, by = "PRESENT_USE") %>%
+    left_join(pub_parcel_ready, by = "PIN") %>%
+    mutate(ASSESSOR_PUB_LIST_LGL = if_else(is.na(ASSESSOR_PUB_LIST_LGL),FALSE,ASSESSOR_PUB_LIST_LGL)) %>%
     select(PIN,
            PROP_NAME,
-           PROP_TYPE = PROP_TYPE_DESC, 
+           PROP_TYPE = PROP_TYPE_DESC,
            ASSESSOR_PUB_LIST_LGL,
            DISTRICT_NAME,
            CURRENT_ZONING,
@@ -439,45 +442,45 @@ make_parcel_ready <- function(lu, prop_type, tax_status,tax_reason, pub_parcel, 
            RESTRICTIVE_SZ_SHAPE,
            PCNT_UNUSABLE,
            CONTAMINATION,
-           HISTORIC_SITE:OTHER_PROBLEMS 
+           HISTORIC_SITE:OTHER_PROBLEMS
     )
-     
+
   # MAKE ACCT_READY
-  
-  acct_frmt <- acct %>% 
+
+  acct_frmt <- acct %>%
     mutate(MAJOR = str_pad(string = MAJOR,width = 6,side = "left",pad = "0"),
            MINOR = str_pad(string = MINOR,width = 4,side = "left",pad = "0"),
-           PIN = str_c(MAJOR,MINOR)) %>% 
+           PIN = str_c(MAJOR,MINOR)) %>%
     rename(TAX_STATUS = TAXSTAT,
-           TAX_REASON = TAXVALREASON) %>% 
-    left_join(tax_status, by = "TAX_STATUS") %>% 
-    left_join(tax_reason, by = "TAX_REASON") %>%  
-    select(PIN, 
+           TAX_REASON = TAXVALREASON) %>%
+    left_join(tax_status, by = "TAX_STATUS") %>%
+    left_join(tax_reason, by = "TAX_REASON") %>%
+    select(PIN,
            TAXPAYER_NAME = TAXPAYERNAME,
            BILL_YR = BILLYR,
            TAX_STATUS = TAX_STATUS_DESC,
            TAX_REASON = TAX_REASON_DESC,
            APPR_IMPS_VAL = APPRIMPSVAL,
-           APPR_LAND_VAL = APPRLANDVAL, 
+           APPR_LAND_VAL = APPRLANDVAL,
            TAXABLE_IMPS_VAL = TAXABLEIMPSVAL,
            TAXABLE_LAND_VAL = TAXABLELANDVAL
     )
-  
-  acct_ready <- acct_frmt %>% 
-    miscgis::subset_duplicated("PIN") %>% 
-    group_by(PIN) %>% 
+
+  acct_ready <- acct_frmt %>%
+    miscgis::subset_duplicated("PIN") %>%
+    group_by(PIN) %>%
     drop_na %>% # remove any duplicates PIN records with NAs
     slice(1) %>%  # take the first record and discard the rest
-    ungroup %>% 
-    bind_rows(subset_duplicated(acct_frmt,"PIN",notin = TRUE)) %>% 
+    ungroup %>%
+    bind_rows(subset_duplicated(acct_frmt,"PIN",notin = TRUE)) %>%
     arrange(PIN)
-  
+
   # MAKE PARCEL_READY
-  
+
   obj_list <- list(acct_ready,p_addr_ready,p_ready,p_sf_ready)
-  
-  parcel_ready <- obj_list %>% 
-    reduce(.f = inner_join, by = "PIN") %>% 
+
+  parcel_ready <- obj_list %>%
+    reduce(.f = inner_join, by = "PIN") %>%
     st_as_sf()
 
   return(parcel_ready)
