72e2542d37040fd1fe1f46c9c0ef36e27da426ea
Tiernan Martin <tiernan.martin@gmail.com>
2018-03-17 18:46:55 -0700

    minor edit

--- 2-communication/1-rscripts/tmp/tmp-view-inventory-suitable.R
+++ 2-communication/1-rscripts/tmp/tmp-view-inventory-suitable.R
@@ -2,6 +2,8 @@ library(mapview)
 
 loadd(inventory_suitable)
 
+loadd(suitability_criteria)
+
 tmp <- inventory_suitable %>% 
   st_set_geometry("geometry") %>% 
   select(-geom_pt) %>% 
@@ -11,8 +13,21 @@ tmp <- inventory_suitable %>%
             OWNER_CATEGORY,
             TYPE = factor(case_when(OWNER_CATEGORY %in% c("non-profit","uncategorized") ~ OWNER_CATEGORY, 
                                     TRUE ~ "public")),
+            SUIT_PARCEL_AREA,
+            SUIT_PARCEL_AREA_RATIO,
             PRESENT_USE,
             HELPERS_URL_PARCEL_VIEWER)
 
 
-mapview(tmp, zcol = "TYPE", legend = TRUE)
+tmp2 <- tmp %>% filter(SUIT_PARCEL_AREA <= set_units(10, acre))
+
+tmp3 <- tmp %>% filter(SUIT_PARCEL_AREA <= set_units(.125, acre))
+
+
+qplot(data = tmp, x = as.double(SUIT_PARCEL_AREA))
+
+qplot(data = tmp2, x = as.double(SUIT_PARCEL_AREA))
+
+qplot(data = tmp3, x = as.double(SUIT_PARCEL_AREA))
+
+mapview(tmp3, zcol = "SUIT_PARCEL_AREA", legend = TRUE)
