47402b06d8d2fc3cdab912aa0ab1fd5cda35437a
Tiernan Martin <tiernan.martin@gmail.com>
2018-03-17 17:49:53 -0700

    Add `SUITABLE_PARCEL_AREA_LGL` to `SUITABLE_LGL`

--- 2-communication/1-rscripts/03-drake-commands.R
+++ 2-communication/1-rscripts/03-drake-commands.R
@@ -1859,7 +1859,7 @@ make_criteria_undevelopable_present_use <- function(){
 
 make_criteria_area <- function(){ 
   
-  criteria_area <- list("area" = set_units(as.integer(10), acre))  # This is an educated-guess placeholder and may need to be adjusted 
+  criteria_area <- list("area" = set_units(as.integer(40), acre))  # This is an educated-guess placeholder and may need to be adjusted 
   
   return(criteria_area)
   
@@ -2092,7 +2092,7 @@ make_suitability <- function(parcel_ready, suitability_criteria, ...){
       SUITABLE_PRESENT_USE_LGL = if_else(! SUIT_PRESENT_USE %in% suitability_criteria[["undevelopable_presentuse"]],TRUE,FALSE,FALSE),
       SUITABLE_PARCEL_AREA_LGL = if_else(SUIT_PARCEL_AREA <= suitability_criteria[["area"]],TRUE,FALSE,FALSE),
       SUITABLE_PARCEL_AREA_RATIO_LGL = if_else(SUIT_PARCEL_AREA_RATIO >= suitability_criteria[["area_ratio"]],TRUE,FALSE,FALSE),
-      SUITABLE_LGL = SUITABLE_OWNER_LGL & SUITABLE_WATER_OVERLAP_LGL & SUITABLE_WITHIN_UGA_LGL & SUITABLE_ZONING_CONSOL_20_LGL & SUITABLE_PRESENT_USE_LGL & SUITABLE_PARCEL_AREA_RATIO_LGL
+      SUITABLE_LGL = SUITABLE_OWNER_LGL & SUITABLE_WATER_OVERLAP_LGL & SUITABLE_WITHIN_UGA_LGL & SUITABLE_ZONING_CONSOL_20_LGL & SUITABLE_PRESENT_USE_LGL & SUITABLE_PARCEL_AREA_LGL & SUITABLE_PARCEL_AREA_RATIO_LGL
     ) %>% 
     st_sf
   
