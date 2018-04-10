efa3cc8a9663db7ed5c204006ad5dcc9ea3eb406
Tiernan Martin <tiernan.martin@gmail.com>
2018-04-10 15:19:22 -0700

    create `make_env_restrictions()` (closes #70)

--- 2-communication/1-rscripts/03-drake-commands.R
+++ 2-communication/1-rscripts/03-drake-commands.R
@@ -736,6 +736,31 @@ make_parcel_acct_ready <- function(acct, tax_status, tax_reason){
   return(parcel_acct_ready)
 }
 
+# COMMAND: MAKE_PARCEL_ENV_READY ----
+
+make_parcel_env_ready <- function(env_restrictions){
+  
+  env_frmt <- env_restrictions %>% 
+    rename_all(to_screaming_snake_case) %>%   
+    filter(!is.na(TYPE)) %>% 
+    transmute(PIN = make_pin(MAJOR, MINOR),
+              TYPE = to_parsed_case(TYPE) %>% toupper %>% str_replace_all("_"," "), 
+              PCNT_AFFECTED,
+              PCT = if_else(PCNT_AFFECTED>0, str_c(PCNT_AFFECTED,"%"),""),
+              RESTRICTION = str_c(TYPE, " (",PCT,")") %>% str_remove_all("\\s\\(\\)"))
+  
+  env_res <- env_frmt %>% 
+    group_by(PIN) %>%  
+    nest() %>% 
+    transmute(PIN,
+              ENV_RESTRICTIONS = map_chr(data, ~ pluck(.x,"RESTRICTION") %>% str_c(collapse = ", "))) 
+  
+  parcel_env_ready <- env_res
+  
+  return(parcel_env_ready)
+  
+}
+
 # COMMAND: MAKE_PARCEL_READY ----
 
 make_parcel_ready <- function(parcel_addr_ready, ...){
