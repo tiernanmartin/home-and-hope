4619141107f3df749ba0c3bcf642413d7af558fa
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-11 22:31:22 -0700

    add str_unique_lower()

--- 2-communication/1-rscripts/02-drake-functions.R
+++ 2-communication/1-rscripts/02-drake-functions.R
@@ -32,11 +32,22 @@ safe_divide <- function(x,y){
 }
 
 
+# FUNCTION: STR_UNIQUE_LOWER ----------------------------------------------
+
+str_unique_lower <- function(x){ 
+  
+  string <- str_c(unique(str_to_lower(str_replace(x,"_"," "))),collapse = ", ")
+  
+  if(length(string) == 0){return("none")}else(return(string))
+  
+}
+
+
 # FUNCTION: STR_COUNT_FACTOR ----
 
 str_count_factor <- function(x){ 
   
- if(length(x)==1 & all(is.na(x))){return(NA_character_)}else{
+  if(length(x)==1 & all(is.na(x))){return(NA_character_)}else{
     x_summary <- summary(factor(x))
     
     summary_string <- map2_chr(names(x_summary),x_summary,~ str_c(.x,": ",.y)) %>% str_c(collapse = "; ")
@@ -63,15 +74,15 @@ drive_get_datetime_modified <- function(dr_id_string){
 # FUNCTION: ST_AREA_RATIO ----
 
 st_area_ratio <- function(x){ 
-    
-    x$poly_area <- st_area(x)
-    x$min_bound_cicle <- lwgeom::st_minimum_bounding_circle(x, nQuadSegs = 10)
-    x$min_bound_cicle_area <- st_area(x$min_bound_cicle)
-    x$area_ratio <- as.double(x$poly_area/x$min_bound_cicle_area)
-    
-    return(x$area_ratio)
-    
-  }
+  
+  x$poly_area <- st_area(x)
+  x$min_bound_cicle <- lwgeom::st_minimum_bounding_circle(x, nQuadSegs = 10)
+  x$min_bound_cicle_area <- st_area(x$min_bound_cicle)
+  x$area_ratio <- as.double(x$poly_area/x$min_bound_cicle_area)
+  
+  return(x$area_ratio)
+  
+}
 
 
 # FUNCTION: STR_CLEAN_UPPER ----
@@ -84,7 +95,7 @@ str_clean_upper <- function(x){str_to_upper(str_trim(str_squish(str_replace_all(
 
 gs_read_all <- function(ss, delay_length = 5){ 
   ws_names <- gs_ws_ls(ss)
-   
+  
   
   gs_read_delayed <- function(ss, ws){
     result <- gs_read(ss, ws)
@@ -113,32 +124,32 @@ recode_logical_01 <- function(x){ if_else(x %in% 1,TRUE,FALSE,missing = NA)}
 is_logical_yesno <- function(x){
   
   strings <- list("yes", "no") %>% 
-  map(~ list(to_screaming_snake_case,to_snake_case,to_upper_camel_case) %>% 
-        invoke_map_chr(.x) 
-        ) %>% flatten_chr() %>% 
+    map(~ list(to_screaming_snake_case,to_snake_case,to_upper_camel_case) %>% 
+          invoke_map_chr(.x) 
+    ) %>% flatten_chr() %>% 
     prepend(NA_character_)
   
   all(unique(x) %in% strings)
   
-  }
+}
 
 recode_logical_yesno <- function(x){ 
   
   yes <- map("yes",~ list(to_screaming_snake_case,to_snake_case,to_upper_camel_case) %>% 
-        invoke_map_chr(.x) 
-        ) %>% flatten_chr()
+               invoke_map_chr(.x) 
+  ) %>% flatten_chr()
   
   if_else(x %in% yes,TRUE,FALSE,missing = NA)
   
-  }
+}
 
 
 # FUNCTION: NAME_TBL_VECTOR ----
 name_tbl_vector <- function(x, name, value){
   x %>% 
     transpose %>% 
-  {set_names(map_chr(., value), map_chr(.,name))}
-
+    {set_names(map_chr(., value), map_chr(.,name))}
+  
 }
 
 # FUNCTION: ANY_NOT_NA ----
@@ -150,61 +161,61 @@ any_not_na <- function(x){ reduce( map(x, is.na), `+`) != ncol(x)}
 # FUNCTION: EMPTY_AS_NA ----
 
 empty_as_na <- function(x){
-    if("factor" %in% class(x)) x <- as.character(x) ## since ifelse wont work with factors
-    ifelse(as.character(x)!="", x, NA_character_)
+  if("factor" %in% class(x)) x <- as.character(x) ## since ifelse wont work with factors
+  ifelse(as.character(x)!="", x, NA_character_)
 }
 
 
 # FUNCTION: EXTRACT_TARGET_PATHS ----
 
 extract_target_paths <- function(plan){
- 
+  
   paths <- pluck(plan,"target") %>% map_chr(str_replace_all, pattern = "'", replacement = "")
-
+  
   shp_pattern <- "\\.shp$"
   
   paths_contain_shapefiles <- any(str_detect(paths, shp_pattern))
   
- if(paths_contain_shapefiles){ 
-   
-   shapefile_exts <- c(".shx",".dbf",".prj") 
-   
-   shps <- keep(paths, ~ str_detect(.,shp_pattern)) 
-   
-   new_paths <- shps %>% 
-     map(str_replace, pattern = shp_pattern, replacement = shapefile_exts) %>% 
-     flatten_chr %>% 
-     c(paths)
-   
-   return(new_paths)
-   
- }else{ return(paths) }
-
+  if(paths_contain_shapefiles){ 
+    
+    shapefile_exts <- c(".shx",".dbf",".prj") 
+    
+    shps <- keep(paths, ~ str_detect(.,shp_pattern)) 
+    
+    new_paths <- shps %>% 
+      map(str_replace, pattern = shp_pattern, replacement = shapefile_exts) %>% 
+      flatten_chr %>% 
+      c(paths)
+    
+    return(new_paths)
+    
+  }else{ return(paths) }
+  
 }
 
 
 
 # FUNCTION: INTEGER_DUMMY ----
-  integer_dummy <- function(n, mean = 0, sd = 1){
-    
-    x <- as.integer(round(abs(rnorm(n, mean , sd)),0))
-   
-    return(x)
-  }
+integer_dummy <- function(n, mean = 0, sd = 1){
+  
+  x <- as.integer(round(abs(rnorm(n, mean , sd)),0))
+  
+  return(x)
+}
 
 # FUNCTION: SPATIAL_DUMMY ----
-  spatial_dummy <- function(n, mean = 0, sd = 1){
-    
-    jitter <- abs(rnorm(n, mean = 0, sd = 1)) 
-    
-    x <- rnorm(n, mean , sd) 
-    
-    x[x<0] <- .1
-    
-    x <- round(x + jitter,2)
-    
-    return(x)
-  }
+spatial_dummy <- function(n, mean = 0, sd = 1){
+  
+  jitter <- abs(rnorm(n, mean = 0, sd = 1)) 
+  
+  x <- rnorm(n, mean , sd) 
+  
+  x[x<0] <- .1
+  
+  x <- round(x + jitter,2)
+  
+  return(x)
+}
 
 # FUNCTION: DRAKE_HERE ----
 drake_here <- function(x) {
@@ -213,8 +224,8 @@ drake_here <- function(x) {
 # FUNCTION: WITHIN_RANGE ----
 `%within_range%` <- function(x,range){ 
   between(x,min(range),max(range))
-          
-          } 
+  
+} 
 
 # FUNCTION: LESSER_OF ----
 lesser_of <- function(x,y){
@@ -223,7 +234,7 @@ lesser_of <- function(x,y){
   
   if_else(x<= y, x,y, missing = x)
   
-  } 
+} 
 
 # FUNCTION: MAKE_PARSE_LU_STRING ----
 parse_lu_string <- function(string, col_sep, row_sep, join_name, long_name){ 
